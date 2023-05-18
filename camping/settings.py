78b48507f8e44310118978878b35d5c0bd29e94f
JiSuMun <mjs3827@naver.com>
2023-05-19 06:14:12 +0900

    add domain

--- camping/settings.py
+++ camping/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['port-0-camping-13aenn2blhtl40fz.sel4.cloudtype.app', '127.0.0.1', 'localhost', '5camping.kro.kr']
+ALLOWED_HOSTS = ['port-0-camping-13aenn2blhtl40fz.sel4.cloudtype.app', '127.0.0.1', 'localhost', '5camping.kro.kr', 'campinggajo.r-e.kr',]
 
 
 # Application definition
